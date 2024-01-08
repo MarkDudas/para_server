385fa1a85ece3680aa51f7ed136c559b5191a0f8
Mark Dudas <110264939+MarkDudas@users.noreply.github.com>
2024-01-08 19:31:30 +0800

    try

--- src/controller/OpenAiController.js
+++ src/controller/OpenAiController.js
@@ -34,9 +34,19 @@ const getChatReply = async (req, res) => {
     "email": "john.smith@example.com",
     "skills": ["Driving"],
     "job_applying": "Driver",
-    "experience": " 2years of driving 10 wheeler truck"
+    "experience": " 2 years of driving 10 wheeler truck"
     "rank": 85,
     "how": "it is 85 because he has a drivers license, high school diploma, can drive 10-wheeler truck, only 2 years experience which does not match the 5 years requirements",
+  }
+  other example depending on applicant resume
+  {
+    "name": "John Smith",
+    "email": "john.smith@example.com",
+    "skills": ["Driving"],
+    "job_applying": "Driver",
+    "experience": " 5 years of driving 10 wheeler truck"
+    "rank": 100,
+    "how": "It is 100 because he met all the job qualification",
   }`;
 
   const { jobQualifications } = req.query;
